6b4c8406bf44ef4356ff34f1ab9498704945c0ef
Crownmoviestudio <91334125+Crownmoviestudio@users.noreply.github.com>
2024-02-15 10:37:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/918088529/MdiskSearchV1.git /MdiskSearchV1
-cd /MdiskSearchV1
+git clone https://github.com/Crownmoviestudio/DiskDom.git /DiskDom
+cd /DiskDom
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
